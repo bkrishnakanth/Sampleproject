03c62f7843faac3cd78e99598dfc2723652e249d
bkrishnakanth <bkrishnakanth456@gmail.com>
2022-12-02 19:25:09 +0530

    First commit

---
+++ ABC1.java
@@ -0,0 +1,6 @@
+class ABC1{
+public static void main(String a[])
+{
+System.out.println("Abc-1");
+}
+}
---
+++ Abc.java
@@ -0,0 +1,13 @@
+class Student { 
+	// This is a method in class Student
+    public void print() { 
+		System.out.println("Hi! I am class STUDENT."); 
+    } 
+}
+
+public class Abc{ 
+    public static void main(String args[]){
+Student s=new Student();
+s.print();
+}
+}
