25f82863f2db00b1db9648e018c8e46b2a5362ed
bkrishnakanth <bkrishnakanth456@gmail.com>
2022-12-02 20:49:13 +0530

    Changes made in Abc.java update student2 sop

--- Abc.java
+++ Abc.java
@@ -1,7 +1,9 @@
 class Student { 
 	// This is a method in class Student
     public void print() { 
-		System.out.println("Hi! I am class STUDENT."); 
+		System.out.println("Hi! I am class STUDENT.");
+System.out.println("Hi! I am class STUDENT2."); 
+           
     } 
 }
 
